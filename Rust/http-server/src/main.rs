7c552e9e7df5a32d9b7a676dce3c94f6ef4321fb
ymt117 <yamamto117@gmail.com>
2020-06-26 10:04:14 +0900

    Add multi-thread

--- Rust/http-server/src/main.rs
+++ Rust/http-server/src/main.rs
@@ -1,15 +1,19 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:12345").unwrap();
 
     for stream in listener.incoming() {
-       let mut stream = stream.unwrap();
-       let mut buffer = [0; 1024];
-       stream.read(&mut buffer).unwrap();
+        let mut stream = stream.unwrap();
 
-       stream.write(String::from_utf8_lossy(&buffer[..]).as_bytes());
+        thread::spawn(move || {
+            let mut buffer = [0; 1024];
+            stream.read(&mut buffer).unwrap();
+
+            stream.write(String::from_utf8_lossy(&buffer[..]).as_bytes());
+        });
     }
 }
 
