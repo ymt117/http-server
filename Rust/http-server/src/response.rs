73d27b5db727c80400b35f9259b0e9448c61f271
ymt117 <yamamto117@gmail.com>
2020-07-17 15:53:34 +0900

    パスを整形する関数名を変更した

--- Rust/http-server/src/response.rs
+++ Rust/http-server/src/response.rs
@@ -5,7 +5,7 @@ use std::io::ErrorKind;
 
 // /root以下のファイルのみアクセスを許可できるように
 // リクエストされたパスを整形する
-fn get_path(path: String) -> String {
+fn get_full_path(path: String) -> String {
     let root = "root";
     if path == "/" {
         return format!("{}{}", root, "/index.html");
@@ -17,7 +17,7 @@ fn get_path(path: String) -> String {
 pub fn build_response(mut path: String) -> (String, String, Vec<u8>) {
 	let mut status_line = "HTTP/1.1 200 OK\r\n";
 
-    path = get_path(path);
+    path = get_full_path(path);
     println!("path: {}", path);
 
 	let ext = path.split(".").last().unwrap_or("");// extention:拡張子
@@ -32,7 +32,7 @@ pub fn build_response(mut path: String) -> (String, String, Vec<u8>) {
 			// 404 Errorの処理を書く
 			status_line = "HTTP/1.1 404 NOT FOUND\r\n";
 			content_type = parser::ContentType::from_file_ext("html");
-			File::open(get_path("/404.html".to_string())).unwrap()
+			File::open(get_full_path("/404.html".to_string())).unwrap()
 		}
 		Err(e) => {
 			panic!("There was a ploblem opening the file: {:?}", e)
