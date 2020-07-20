62f0ba89d023c2f58e2fcddfa029539aa438b561
ymt117 <yamamto117@gmail.com>
2020-07-20 14:36:51 +0900

    指摘箇所をリファクタリングした

--- Rust/http-server/src/response.rs
+++ Rust/http-server/src/response.rs
@@ -5,7 +5,7 @@ use std::io::ErrorKind;
 
 // /root以下のファイルのみアクセスを許可できるように
 // リクエストされたパスを整形する
-fn get_full_path(path: String) -> String {
+fn to_full_path(path: String) -> String {
     let root = "root";
     if path == "/" {
         return format!("{}{}", root, "/index.html");
@@ -14,25 +14,25 @@ fn get_full_path(path: String) -> String {
     return format!("{}{}", root, path)
 }
 
-pub fn build_response(mut path: String) -> (String, String, Vec<u8>) {
+pub fn build_response(path: String) -> (String, String, Vec<u8>) {
 	let mut status_line = "HTTP/1.1 200 OK\r\n";
 
-    path = get_full_path(path);
-    println!("path: {}", path);
+    let full_path = to_full_path(path);
+    println!("path: {}", full_path);
 
-	let ext = path.split(".").last().unwrap_or("");// extention:拡張子
-	let mut content_type = parser::ContentType::from_file_ext(ext);
+	let extention = full_path.split(".").last().unwrap_or("");// extention:拡張子
+	let mut content_type = parser::ContentType::from_file_ext(extention);
 	//println!("path: {}", path);
 	//println!("content type: {:?}", content_type);
 	
-	let file = File::open(path);
+	let file = File::open(full_path);
 	let mut file = match file {
 		Ok(file) => file,
 		Err(ref e) if e.kind() == ErrorKind::NotFound => {
 			// 404 Errorの処理を書く
 			status_line = "HTTP/1.1 404 NOT FOUND\r\n";
 			content_type = parser::ContentType::from_file_ext("html");
-			File::open(get_full_path("/404.html".to_string())).unwrap()
+			File::open(to_full_path("/404.html".to_string())).unwrap()
 		}
 		Err(e) => {
 			panic!("There was a ploblem opening the file: {:?}", e)
